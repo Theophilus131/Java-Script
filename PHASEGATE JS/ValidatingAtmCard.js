575791420df81444b6383eff667eef31eaecdca0
Theophilus131 <theophilusumar27@gmail.com>
2025-05-30 11:54:44 +0100

    initial

---
+++ PHASEGATE JS/ValidatingAtmCard.js
@@ -0,0 +1,10 @@
+let cardNumber = [3,4,0,1,2,6, 7, 8,9,4,5,2,7,8,9,5];
+
+function validatingAtm(array){
+
+cardIssuer = { "visaCard", "masterCard", "discover", "americaExpress", };
+
+for(let index = 0; index < cardNumber.length; index++){
+	
+	}
+	
