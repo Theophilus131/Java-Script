c81046369ef2e6802caba917df983ce6c0d76db8
Theophilus131 <theophilusumar27@gmail.com>
2025-06-11 10:08:05 +0100

    task

--- strings.js
+++ strings.js
@@ -1,6 +1,15 @@
 //console.log("ASAP frontend"[12]);
 //console.log("Asap".length);
 
-let string = "Asap frontend "
+//let string = "Asap frontend "
 
-console.log(string.length-1)
+//console.log(string.length-1)
+
+// curreny converter
+// aud = usd * 1.5
+
+let USD = 1200
+
+let AUD = 1200 * 1.5
+
+console.log("AUD: " + AUD);
