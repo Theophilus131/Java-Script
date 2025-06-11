9cf4c26450fa11fa786166b38c3ba2305292acf5
Theophilus131 <theophilusumar27@gmail.com>
2025-06-11 19:34:12 +0100

    object

---
+++ array.js
@@ -0,0 +1,11 @@
+let array = ["Apples", "Bananas", "Eggs", "Milk"]
+
+console.log(array[0])
+
+console.log(array[array.length -1])
+
+//array methods
+
+array.push("Bread")
+
+console.log(array)
---
+++ object.js
@@ -0,0 +1,13 @@
+// object
+
+let user = {
+
+email: "example@gmail.com",
+password: "test1234",
+name: "john joe",
+discord: "examplediscord",
+subscription: "vip"
+
+}
+
+console.log(user.email)
