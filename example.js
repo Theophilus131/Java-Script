bfc616c7f47b5810ac920021f26e070c7fc4bc5b
Theophilus131 <theophilusumar27@gmail.com>
2025-06-11 13:04:49 +0100

    loop

---
+++ example.js
@@ -0,0 +1,23 @@
+// write a program that loops 1 to 15. if the number is odd, print "asap"
+// if the number is even, print "frontend"
+// if the number is dividible by 5, print "asap frontend"
+
+
+
+
+for(let i = 1; i <= 15; i++){
+
+if(15 % 5 === 0){
+console.log(`${i}-  Asap frontend`);
+	}
+
+else if(i % 2 === 1){
+console.log(`${i} - Asap`);
+ }
+else{
+console.log(`${i} - frontend `);
+}
+
+
+}
+
